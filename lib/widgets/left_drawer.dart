f2c60b66c2e061d4dbdcda17c583e61403e1c90f
rexy <rexy.adrian.f@gmail.com>
2025-11-24 02:32:16 +0700

    feat: fitur autentikasi dan integrasi dengan api django

---
+++ lib/widgets/left_drawer.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:gymbuddy/screens/menu.dart';
+
+class LeftDrawer extends StatelessWidget {
+  const LeftDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Column(
+              children: [
+                Text(
+                  'GymBuddy',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(10)),
+                Text("Say no to olahraga ribet! 💪🏋️‍♂️",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          ListTile(
+            leading: const Icon(Icons.home_outlined),
+            title: const Text('Home'),
+            // Bagian redirection ke MyHomePage
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MyHomePage(),
+                )
+              );
+            },
+          ),
+
+          /* note dari Rexy
+          * TEMPLATE BUAT BAGIAN DRAWER
+          * 1. COPY SELURUH ISI ListTile(
+          * ...
+          * ),
+          * 2. ubah title dan routing pada builder: (context) => <page kalian>
+          */
+          ListTile(
+            leading: const Icon(Icons.post_add),
+            title: const Text('Add'),
+            // Bagian redirection ke page, misal form page
+            onTap: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MyHomePage(), // <= ubah ini, misal PlannerPage(), CommunityPage(), dll.
+                  )
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
